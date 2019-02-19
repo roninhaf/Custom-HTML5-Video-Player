1d52e16e9bb97647f61c11c63d9eb2e65949578e
roninhaf <roninhaf@gmail.com>
2019-02-19 14:43:12 -0500

    Allowed player to go fullscreen

--- script.js
+++ script.js
@@ -34,14 +34,14 @@ function handleProgress() {
 }
 
 function toggleFullscreen() {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) { /* Firefox */
-      video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-      video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) { /* IE/Edge */
-      video.msRequestFullscreen();
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) { /* Firefox */
+      player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) { /* IE/Edge */
+      player.msRequestFullscreen();
     }
   }
 
